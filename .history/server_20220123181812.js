fa74505add647f266a17710b6e2931e7b8e28b45
brenhamp <bpozzi@trinity.edu>
2022-01-23 18:50:55 -0600

    added heroku package

---
+++ .history/server_20220123181812.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require("path");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+const directory = path.join(__dirname, "/public");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(directory, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push((newNote + "\n"));
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved! ", newNote);
+    res.json(savedNotes);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(selectedNote => {
+        return selectedNote.id != noteID;
+    })
+
+    for (selectedNote of savedNotes) {
+        selectedNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now on port ${PORT}!`);
+});
